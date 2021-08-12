d0cef73c90b601703e84c01f0039db76bbc7c2a7
vytcka4 <vytautas.freelance@gmail.com>
2021-08-12 23:36:52 +0300

    commit

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-// const julia = [3, 5, 2, 12, 7];
-// const kate = [4, 1, 15, 8, 3];
+const julia = [3, 5, 2, 12, 7];
+const kate = [4, 1, 15, 8, 3];
 
-// const chechDogs = function (juliaDogs, kateDogs) {
-//   const juliaDoges = julia;
-//   const kateDoges = kate;
-//   juliaDoges.splice(0, 1);
-//   juliaDoges.splice(2, 3);
-//   kateDoges.splice(0, 1);
-//   kateDoges.splice(2, 3);
-//   const allDogs = juliaDoges.concat(kateDoges);
-//   console.log(allDogs);
-//   console.log(juliaDoges.length);
-//   for (let i = 0; i < allDogs.length; i++) {
-//     if (allDogs[i] >= 3) {
-//       console.log(`"Dog number ${i + 1}
-//       is an adult, and is ${allDogs[i]} years old"`);
-//     } else console.log(`Dog number ${i + 1} is still puppy`);
-//   }
-// };
-// chechDogs(...julia, ...kate);
+const chechDogs = function (juliaDogs, kateDogs) {
+  const juliaDoges = julia;
+  const kateDoges = kate;
+  juliaDoges.splice(0, 1);
+  juliaDoges.splice(2, 3);
+  kateDoges.splice(0, 1);
+  kateDoges.splice(2, 3);
+  const allDogs = juliaDoges.concat(kateDoges);
+  console.log(allDogs);
+  console.log(juliaDoges.length);
+  for (let i = 0; i < allDogs.length; i++) {
+    if (allDogs[i] >= 3) {
+      console.log(`"Dog number ${i + 1}
+      is an adult, and is ${allDogs[i]} years old"`);
+    } else console.log(`Dog number ${i + 1} is still puppy`);
+  }
+};
+chechDogs(...julia, ...kate);
 const ages = [5, 2, 4, 1, 15, 8, 3];
 
 calcAverageHumanAge = function (ages) {
